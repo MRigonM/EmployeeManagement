446177658a5fbaf4c297d653c58f6f263f470774
Rigon <rigonmej@gmail.com>
2025-09-27 23:24:51 +0200

    Configure EF Core and Identity with ApplicationDbContext, add initial migration

---
+++ EmployeeManagement/Extensions/ApplicationServices.cs
@@ -0,0 +1,26 @@
+﻿using EmployeeManagement.Domain.Entities;
+using EmployeeManagement.Infrastructure.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeManagement.Extensions;
+
+public static class ApplicationServices
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<ApplicationDbContext>(options =>
+        {
+            options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+        });
+
+        services.AddIdentityCore<AppUser>(options =>
+            {
+                options.SignIn.RequireConfirmedEmail = false;
+            })
+            .AddEntityFrameworkStores<ApplicationDbContext>()
+            .AddDefaultTokenProviders();
+
+        return services;
+    }
+}

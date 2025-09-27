99c03a0dbad0d125d8738690c754c68b2d80b741
Rigon <rigonmej@gmail.com>
2025-09-27 22:56:00 +0200

    Add AppUser entity, ISoftDeletion interface and ApplicationDbContext with soft delete filters

---
+++ EmployeeManagement.Domain/Entities/AppUser.cs
@@ -0,0 +1,15 @@
+﻿using EmployeeManagement.Domain.Interfaces;
+using Microsoft.AspNetCore.Identity;
+
+namespace EmployeeManagement.Domain.Entities;
+
+public class AppUser : IdentityUser, ISoftDeletion
+{
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+    public bool IsDeleted { get; set; }
+    public DateTimeOffset? DeletedAt { get; set; }
+}
---
+++ EmployeeManagement.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using EmployeeManagement.Domain.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace EmployeeManagement.Infrastructure.Data;
+
+public class ApplicationDbContext : IdentityDbContext<AppUser>
+{
+    private readonly IConfiguration _configuration;
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options, IConfiguration configuration) : base(options)
+    {
+        _configuration = configuration;
+    }
+    
+    public DbSet<Department> Departments { get; set; }
+    public DbSet<Employee> Employees { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Employee>().HasQueryFilter(e => !e.IsDeleted);
+        modelBuilder.Entity<Department>().HasQueryFilter(d => !d.IsDeleted);
+        modelBuilder.Entity<AppUser>().HasQueryFilter(u => !u.IsDeleted);
+    }
+}

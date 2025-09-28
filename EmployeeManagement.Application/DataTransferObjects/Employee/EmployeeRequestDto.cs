919f3289d9a3268f7a4f3fb14b9de6150fe73062
Rigon <rigonmej@gmail.com>
2025-09-28 15:13:30 +0200

    Add DTOs for Department and Employee entities

---
+++ EmployeeManagement.Application/DataTransferObjects/Employee/EmployeeRequestDto.cs
@@ -0,0 +1,10 @@
+﻿namespace EmployeeManagement.Application.DataTransferObjects.Employee;
+
+public class EmployeeRequestDto
+{
+    public string Name { get; set; } = string.Empty;
+    public string Surname { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string? PhoneNumber { get; set; }
+    public int DepartmentId { get; set; }
+}

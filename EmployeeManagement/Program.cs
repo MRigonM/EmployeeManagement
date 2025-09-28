c2cfe94efbbbcc1a986822d46ab78390675237c8
Rigon <rigonmej@gmail.com>
2025-09-28 23:13:24 +0200

    Add JWT auth and Swagger; update ApplicationServices

--- EmployeeManagement/Program.cs
+++ EmployeeManagement/Program.cs
@@ -1,19 +1,24 @@
 using EmployeeManagement.Extensions;
+using EmployeeManagement.Infrastructure.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddApplicationServices(builder.Configuration);
-
+builder.Services.AddJwtAuthentication(builder.Configuration);
+builder.Services.AddSwaggerDocumentation();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    await DataSeeder.SeedAsync(scope.ServiceProvider);
+}
+
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerDocumentation();
 }
 
 app.UseHttpsRedirection();

ec472f76787da286c8976b303da93adae5cd3ea8
Rigon <rigonmej@gmail.com>
2025-09-28 02:28:43 +0200

    Add DepartmentRepository and UnitOfWork implementations

---
+++ EmployeeManagement.Domain/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,6 @@
+﻿namespace EmployeeManagement.Domain.Interfaces;
+
+public interface IUnitOfWork
+{
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+}
---
+++ EmployeeManagement.Infrastructure/Repositories/UnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using EmployeeManagement.Domain.Interfaces;
+using EmployeeManagement.Infrastructure.Data;
+
+namespace EmployeeManagement.Infrastructure.Repositories;
+
+public class UnitOfWork: IUnitOfWork
+{
+    private readonly ApplicationDbContext _dbContext;
+
+    public UnitOfWork(ApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        return await _dbContext.SaveChangesAsync(cancellationToken);
+    }
+}

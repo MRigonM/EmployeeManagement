a09e10ad6122c4eedf600ca9d5b840ee862acec4
Rigon <rigonmej@gmail.com>
2025-09-29 00:06:55 +0200

    Add validators for department and employee requests

---
+++ EmployeeManagement.Application/Validators/EmployeeRequestValidator.cs
@@ -0,0 +1,30 @@
+﻿using EmployeeManagement.Application.DataTransferObjects.Employee;
+using FluentValidation;
+
+namespace EmployeeManagement.Application.Validators
+{
+    public class EmployeeRequestValidator : AbstractValidator<EmployeeRequestDto>
+    {
+        public EmployeeRequestValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("Name is required.")
+                .MaximumLength(50).WithMessage("Name cannot exceed 50 characters.");
+
+            RuleFor(x => x.Surname)
+                .NotEmpty().WithMessage("Surname is required.")
+                .MaximumLength(50).WithMessage("Surname cannot exceed 50 characters.");
+
+            RuleFor(x => x.Email)
+                .NotEmpty().WithMessage("Email is required.")
+                .EmailAddress().WithMessage("Invalid email format.");
+
+            RuleFor(x => x.PhoneNumber)
+                .NotEmpty().WithMessage("Phone number is required.")
+                .Matches(@"^\+?\d{7,15}$").WithMessage("Phone number must be 7–15 digits and may start with +.");
+
+            RuleFor(x => x.DepartmentId)
+                .GreaterThan(0).WithMessage("DepartmentId must be greater than 0.");
+        }
+    }
+}

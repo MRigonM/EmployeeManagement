bb14f881a2674e05d7f17852c5b9e8a73b09d491
Rigon <rigonmej@gmail.com>
2025-09-29 14:41:28 +0200

    Refactor and organize validator classes

---
+++ EmployeeManagement.Application/Validators/Employee/EmpoyeeUpdateValidator.cs
@@ -0,0 +1,25 @@
+﻿using EmployeeManagement.Application.DataTransferObjects.Employee;
+using FluentValidation;
+
+namespace EmployeeManagement.Application.Validators.Employee;
+
+public class EmpoyeeUpdateValidator : AbstractValidator<EmployeeCreateDto>
+{
+    public EmpoyeeUpdateValidator()
+    {
+        RuleFor(x => x.Name)
+            .MaximumLength(50).WithMessage("Name cannot exceed 50 characters.");
+
+        RuleFor(x => x.Surname)
+            .MaximumLength(50).WithMessage("Surname cannot exceed 50 characters.");
+
+        RuleFor(x => x.Email)
+            .EmailAddress().WithMessage("Invalid email format.");
+
+        RuleFor(x => x.PhoneNumber)
+            .Matches(@"^\+?\d{7,15}$").WithMessage("Phone number must be 7–15 digits and may start with +.");
+
+        RuleFor(x => x.DepartmentId)
+            .GreaterThan(0).WithMessage("DepartmentId must be greater than 0.");
+    }
+}
